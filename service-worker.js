7dd649d7bb64827de2f4229030b691c2a009bf76
auth <auth1ery@users.noreply.github.com>
2026-03-30 20:19:11 -0600

    uhh

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('app-cache').then(cache => {
+    caches.open('app-cache').then((cache) => {
       return cache.addAll([
         '/',
         '/index.html',
         '/manifest.json',
-        'https://i.imgur.com/RZVHfEq.png'
+        'https://i.imgur.com/RZVHfEq.png',
       ]);
-    })
+    }),
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
-    })
+    }),
   );
 });
